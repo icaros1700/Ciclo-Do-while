5a1a35b7eca22d77f0300e97a02b63790eb5ddc3
william Ruiz <william.a23@gmail.com>
2022-07-07 14:21:52 -0500

    ciclo do while

---
+++ Dowhile/src/Dowhile.java
@@ -0,0 +1,12 @@
+public class Dowhile {
+    public static void main(String[] args) throws Exception {
+        int numeroDo = 2;
+
+        do {
+            System.out.println(numeroDo);
+            numeroDo++;
+
+        } while (numeroDo < 3);
+        
+    }
+}
